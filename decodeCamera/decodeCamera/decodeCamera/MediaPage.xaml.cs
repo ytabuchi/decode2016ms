d52bbb0aee685db4c9d0b8e393f26ca8665b404a
ytabuchi @ Surface <ytabuchi.xlsoft@gmail.com>
2016-05-25 03:45:27 +0900

    camera app commit

---
+++ decodeCamera/decodeCamera/decodeCamera/MediaPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Plugin.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace decodeCamera
+{
+    public partial class MediaPage : ContentPage
+    {
+        public MediaPage()
+        {
+            InitializeComponent();
+        }
+
+        async void buttonClicked(object sender, EventArgs s)
+        {
+            await CrossMedia.Current.Initialize();
+
+            //throw new NotImplementedException();
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                DisplayAlert("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Directory = "Sample",
+                Name = "test.jpg"
+            });
+
+            if (file == null)
+                return;
+
+            //DisplayAlert("File Location", file.Path, "OK");
+            label.Text = string.Format($"File location: {file.Path}");
+
+            image.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+            });
+        }
+    }
+}
